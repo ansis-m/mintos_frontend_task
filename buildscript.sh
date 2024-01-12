e24727afb15f9eea896e16ace7969f99e955dbac
ansis@home <ansis@home>
2024-01-12 09:04:14 +0200

    setup docker-compose and buildscript

---
+++ buildscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#docker logout
+#docker login
+
+docker-compose down
+docker build --cache-from mintos . -t mintos:latest
+docker tag mintos:latest ansism/mintos:latest
+docker push ansism/mintos:latest
+docker-compose up
+
+
