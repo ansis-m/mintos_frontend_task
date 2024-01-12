b75752c93282d87c6520bf69ad78ca4730d4bf83
ansis@home <ansis@home>
2024-01-12 15:12:21 +0200

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrencySelectorComponent } from './currency-selector/currency-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CurrencySelectorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/currency-selector/currency-selector.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-selector',
+  templateUrl: './currency-selector.component.html',
+  styleUrls: ['./currency-selector.component.css']
+})
+export class CurrencySelectorComponent {
+
+}
